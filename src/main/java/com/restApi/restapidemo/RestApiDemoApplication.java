8e189def9c566bd699cf261f6f7965358efc9cad
mino4084 <mino4084@kyobobook.com>
2023-11-16 08:46:05 +0900

    Initial commit

---
+++ src/main/java/com/restApi/restapidemo/RestApiDemoApplication.java
@@ -0,0 +1,41 @@
+package com.restApi.restapidemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.UUID;
+
+@SpringBootApplication
+public class RestApiDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiDemoApplication.class, args);
+	}
+
+}
+
+class Coffee {
+	private final String id;
+	private String name;
+
+	public Coffee(String id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+
+	public Coffee(String name) {
+		this(UUID.randomUUID().toString(), name);
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
