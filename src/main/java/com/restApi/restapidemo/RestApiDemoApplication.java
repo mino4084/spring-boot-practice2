4b373833c50ea6112d77eb69aec1c971bc6c3abe
mino4084 <mino4084@kyobobook.com>
2023-11-16 09:09:55 +0900

    REST API 구현 Coffee 클래스로 기본 GET 메소드 개발(단일, 리스트 타입)

--- src/main/java/com/restApi/restapidemo/RestApiDemoApplication.java
+++ src/main/java/com/restApi/restapidemo/RestApiDemoApplication.java
@@ -2,16 +2,53 @@ package com.restApi.restapidemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @SpringBootApplication
+
 public class RestApiDemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RestApiDemoApplication.class, args);
 	}
 
+}
+
+@RestController
+@RequestMapping("/")
+class RestApiDemoController {
+	private List<Coffee> coffeeList = new ArrayList<>();
+
+	public RestApiDemoController() {
+		coffeeList.addAll(List.of(
+				new Coffee("Cafe Latte"),
+				new Coffee("Americano"),
+				new Coffee("Espresso"),
+				new Coffee("Cappuccino")
+		));
+	}
+
+	//@RequestMapping(value = "/coffee", method = RequestMethod.GET)
+	@GetMapping("/coffee")
+	Iterable<Coffee> getCoffeeList() {
+		return coffeeList;
+	}
+
+	@GetMapping("/coffee/{id}")
+	Optional<Coffee> getCoffeeById(@PathVariable String id) {
+		return coffeeList
+				.stream()
+				.filter(c -> c.getId().equals(id))
+				.findFirst();
+	}
 }
 
 class Coffee {
