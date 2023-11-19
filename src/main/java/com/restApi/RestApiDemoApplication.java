99b23452f0aa7aa8b24caa8d29e395848085bf28
mino4084 <mino4084@kyobobook.com>
2023-11-20 08:30:01 +0900

    스프링 부트 애플리케이션 설정 사용 - @Value - @ConfigurationProperties

--- src/main/java/com/restApi/RestApiDemoApplication.java
+++ src/main/java/com/restApi/RestApiDemoApplication.java
@@ -4,6 +4,9 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,11 +18,17 @@ import java.util.Optional;
 import java.util.UUID;
 
 @SpringBootApplication
-
+@ConfigurationPropertiesScan
 public class RestApiDemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RestApiDemoApplication.class, args);
+	}
+
+	@Bean
+	@ConfigurationProperties(prefix = "droid")
+	Droid createDroid() {
+		return new Droid();
 	}
 
 }
@@ -81,6 +90,86 @@ class RestApiDemoController {
         coffeeRepository.deleteById(id);
 	}
 }
+
+@RestController
+@RequestMapping("/greeting")
+class GreetingController {
+
+	private final Greeting greeting;
+
+	public GreetingController(Greeting greeting) {
+		this.greeting = greeting;
+	}
+
+	@GetMapping
+	String greeting() {
+		return "Hello " + greeting.getName();
+	}
+
+	@GetMapping("/coffee")
+	String greetingCoffee() {
+		return "Hello " + greeting.getCoffee();
+	}
+}
+
+@Component
+@ConfigurationProperties(prefix = "greeting")
+class Greeting {
+	private String name;
+	private String coffee;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCoffee() {
+		return coffee;
+	}
+
+	public void setCoffee(String coffee) {
+		this.coffee = coffee;
+	}
+}
+
+@RestController
+@RequestMapping("/droid")
+class DroidController {
+	private final Droid droid;
+
+	public DroidController(Droid droid) {
+		this.droid = droid;
+	}
+
+	@GetMapping
+	Droid getDroid() {
+		return droid;
+	}
+}
+
+class Droid {
+	private String id, description;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+}
+
 
 interface CoffeeRepository extends CrudRepository<Coffee, String> {}
 
